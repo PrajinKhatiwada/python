6bf2ca39e5ae6df676b3ccb051f6784ffcb0181b
Prajin Khatiwada <prajinkhatiwada@gmail.com>
2024-05-14 14:46:45 +0545

    more on fizzword

--- fizzword.py
+++ fizzword.py
@@ -1,10 +1,21 @@
-num =1;
-while (num<=100):
+num =1
+fizzcount=0
+buzzcount=0
+fizzbuzzcount=0
+while (num<=1000):
     if(num%3==0) and (num%5==0):
         print(str(num) + ":Fizzbuzz!")
+        fizzbuzzcount+=1
     elif(num%3==0):
         print(str(num) + ":Fizz")
+        fizzcount+=1
     elif(num%5==0):
         print(str(num) + ":Buzz")
-    print(num)
+        buzzcount+=1
+    else:
+        print(str(num) + '.')
+   
     num +=1
+print("________________________________")
+print("fizz\tBuzz\tFizzbuzz")
+print(str(fizzcount) + "\t" + str(buzzcount) +"\t" +str(fizzbuzzcount))
